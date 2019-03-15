455fcd859244cf6722b361b61914bca77cdcb654
Jami Dwyer <jdwyer@lo3energy.com>
2019-03-14 22:24:55 -0700

    negative tests

--- services/elections/project/api/elections.py
+++ services/elections/project/api/elections.py
@@ -3,6 +3,8 @@
 
 from flask import Blueprint, jsonify, request
 
+from sqlalchemy import exc
+
 from project.api.models import Election
 from project import db
 
@@ -31,13 +33,28 @@ def election_data():
 @elections_blueprint.route('/elections', methods=['POST'])
 def add_election():
     post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
+    }
+    if not post_data:
+        return jsonify(response_object), 400
     general_election_date = post_data.get('general_election_date')
     primary_election_date = post_data.get('primary_election_date')
     state = post_data.get('state')
-    db.session.add(Election(general_election_date=general_election_date, primary_election_date=primary_election_date, state=state))
-    db.session.commit()
-    response_object = {
-        'status': 'success',
-        'message': f'{general_election_date} was added!'
-    }
-    return jsonify(response_object), 201
+    try:
+        election = Election.query.filter_by(general_election_date=general_election_date, state=state).first()
+        if not election:
+            db.session.add(Election(general_election_date=general_election_date, primary_election_date=primary_election_date, state=state))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{general_election_date} in {state} was added!'
+            }
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That election already exists.'
+            return jsonify(response_object), 400
+    except exc.IntegrityError:
+        db.session.rollback()
+        return jsonify(response_object), 400
--- services/elections/project/tests/test_elections.py
+++ services/elections/project/tests/test_elections.py
@@ -32,8 +32,61 @@ class TestElectionService(BaseTestCase):
 	        )
 	        data = json.loads(response.data.decode())
 	        self.assertEqual(response.status_code, 201)
-	        self.assertIn('November 3, 2020 was added!', data['message'])
+	        self.assertIn('November 3, 2020 in ok was added!', data['message'])
 	        self.assertIn('success', data['status'])
+
+    def test_add_election_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/elections',
+                data=json.dumps({}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_election_invalid_json_keys(self):
+        """
+        Ensure error is thrown if the JSON object does not have a state key.
+        """
+        with self.client:
+            response = self.client.post(
+                '/elections',
+                data=json.dumps({'general_election_date': 'November 3, 2020'}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_election_duplicate_email(self):
+        """Ensure error is thrown if the election already exists."""
+        with self.client:
+            self.client.post(
+                '/elections',
+                data=json.dumps({
+                    'general_election_date': 'November 3, 2020',
+                    'state': 'ok'
+                }),
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/elections',
+                data=json.dumps({
+                    'general_election_date': 'November 3, 2020',
+                    'state': 'ok'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn(
+                'Sorry. That election already exists.', data['message'])
+            self.assertIn('fail', data['status'])
 
 
 if __name__ == '__main__':
